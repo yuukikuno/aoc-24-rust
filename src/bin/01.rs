52908494a7bd5d644dabc5cc9fa5ad46e29a9b27
yuukikuno <kunoyuuki@proton.me>
2026-03-01 16:57:02 +0100

    d01 use hashSets

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u64> {
@@ -24,23 +26,18 @@ pub fn part_two(input: &str) -> Option<u64> {
     let (mut first_list, mut second_list): (Vec<u64>, Vec<u64>) = (vec![], vec![]);
     // build lists
     for line in input.lines() {
-        let mut split = line.split_whitespace();
-        let first = split.next()?.parse::<u64>().unwrap();
-        let second = split.next()?.parse::<u64>().unwrap();
-        first_list.push(first);
-        second_list.push(second);
+        let (first, second) = line.split_once("   ").unwrap();
+        first_list.push(first.parse::<u64>().unwrap());
+        second_list.push(second.parse::<u64>().unwrap());
     }
 
-    first_list.sort();
-    second_list.sort();
-
+    let first_uniq: HashSet<u64> = HashSet::from_iter(first_list.clone());
+    let second_uniq: HashSet<u64> = HashSet::from_iter(second_list.clone());
+    let intersection = first_uniq.intersection(&second_uniq);
     let mut result = 0;
-    for first in first_list {
-        let count = second_list
-            .iter()
-            .filter(|&&second| second == first)
-            .count();
-        result += count as u64 * first;
+    for &x in intersection {
+        let count = second_list.iter().filter(|&&second| second == x).count();
+        result += count as u64 * x;
     }
     Some(result)
 }
